0809080e1e4f8f99fcc003ded606b1080460be3b
hubugui <hubugui@gmail.com>
2016-12-26 16:23:11 +0800

    first commit

---
+++ WTime/Form1.Designer.vb
@@ -0,0 +1,237 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class WTimeForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(WTimeForm))
+        Me.wTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.startButton = New System.Windows.Forms.Button
+        Me.periodText = New System.Windows.Forms.TextBox
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.progressBar = New System.Windows.Forms.ProgressBar
+        Me.timeView = New System.Windows.Forms.TextBox
+        Me.minuteView = New System.Windows.Forms.TextBox
+        Me.Panel1 = New System.Windows.Forms.Panel
+        Me.ToolMenu = New System.Windows.Forms.ToolStripMenuItem
+        Me.TopMostMenu = New System.Windows.Forms.ToolStripMenuItem
+        Me.MenuStrip = New System.Windows.Forms.MenuStrip
+        Me.帮助ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.AboutMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.Panel1.SuspendLayout()
+        Me.MenuStrip.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'wTimer
+        '
+        Me.wTimer.Enabled = True
+        Me.wTimer.Interval = 1000
+        '
+        'startButton
+        '
+        Me.startButton.Font = New System.Drawing.Font("NSimSun", 13.0!, System.Drawing.FontStyle.Bold)
+        Me.startButton.Location = New System.Drawing.Point(284, 11)
+        Me.startButton.Name = "startButton"
+        Me.startButton.Size = New System.Drawing.Size(89, 36)
+        Me.startButton.TabIndex = 2
+        Me.startButton.Text = "开始"
+        Me.startButton.UseVisualStyleBackColor = True
+        '
+        'periodText
+        '
+        Me.periodText.Font = New System.Drawing.Font("NSimSun", 13.0!, System.Drawing.FontStyle.Bold)
+        Me.periodText.ImeMode = System.Windows.Forms.ImeMode.Disable
+        Me.periodText.Location = New System.Drawing.Point(132, 13)
+        Me.periodText.Name = "periodText"
+        Me.periodText.Size = New System.Drawing.Size(76, 32)
+        Me.periodText.TabIndex = 1
+        Me.periodText.Text = "180"
+        Me.periodText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("NSimSun", 13.0!, System.Drawing.FontStyle.Bold)
+        Me.Label1.Location = New System.Drawing.Point(225, 17)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(33, 22)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "秒"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("NSimSun", 13.0!, System.Drawing.FontStyle.Bold)
+        Me.Label2.Location = New System.Drawing.Point(7, 19)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(102, 22)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "循环周期"
+        '
+        'progressBar
+        '
+        Me.progressBar.BackColor = System.Drawing.SystemColors.ControlText
+        Me.progressBar.ForeColor = System.Drawing.Color.Lime
+        Me.progressBar.Location = New System.Drawing.Point(9, 123)
+        Me.progressBar.MarqueeAnimationSpeed = 1
+        Me.progressBar.Name = "progressBar"
+        Me.progressBar.Size = New System.Drawing.Size(363, 39)
+        Me.progressBar.Step = 1
+        Me.progressBar.TabIndex = 3
+        '
+        'timeView
+        '
+        Me.timeView.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.timeView.BackColor = System.Drawing.SystemColors.WindowText
+        Me.timeView.Font = New System.Drawing.Font("Courier New", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.timeView.ForeColor = System.Drawing.Color.Lime
+        Me.timeView.ImeMode = System.Windows.Forms.ImeMode.Disable
+        Me.timeView.Location = New System.Drawing.Point(9, 59)
+        Me.timeView.Margin = New System.Windows.Forms.Padding(3, 1, 1, 3)
+        Me.timeView.Name = "timeView"
+        Me.timeView.Size = New System.Drawing.Size(168, 53)
+        Me.timeView.TabIndex = 3
+        Me.timeView.Text = "000"
+        Me.timeView.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'minuteView
+        '
+        Me.minuteView.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.minuteView.BackColor = System.Drawing.SystemColors.WindowText
+        Me.minuteView.Font = New System.Drawing.Font("Courier New", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.minuteView.ForeColor = System.Drawing.Color.Lime
+        Me.minuteView.ImeMode = System.Windows.Forms.ImeMode.Disable
+        Me.minuteView.Location = New System.Drawing.Point(202, 59)
+        Me.minuteView.Margin = New System.Windows.Forms.Padding(3, 1, 1, 3)
+        Me.minuteView.MinimumSize = New System.Drawing.Size(153, 50)
+        Me.minuteView.Name = "minuteView"
+        Me.minuteView.ReadOnly = True
+        Me.minuteView.Size = New System.Drawing.Size(169, 53)
+        Me.minuteView.TabIndex = 5
+        Me.minuteView.TabStop = False
+        Me.minuteView.Text = "00:00"
+        Me.minuteView.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Panel1
+        '
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel1.Controls.Add(Me.minuteView)
+        Me.Panel1.Controls.Add(Me.progressBar)
+        Me.Panel1.Controls.Add(Me.timeView)
+        Me.Panel1.Controls.Add(Me.startButton)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.periodText)
+        Me.Panel1.Location = New System.Drawing.Point(5, 36)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(384, 170)
+        Me.Panel1.TabIndex = 6
+        '
+        'ToolMenu
+        '
+        Me.ToolMenu.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.TopMostMenu})
+        Me.ToolMenu.Font = New System.Drawing.Font("NSimSun", 13.0!)
+        Me.ToolMenu.Name = "ToolMenu"
+        Me.ToolMenu.Size = New System.Drawing.Size(66, 26)
+        Me.ToolMenu.Text = "工具"
+        '
+        'TopMostMenu
+        '
+        Me.TopMostMenu.AutoToolTip = True
+        Me.TopMostMenu.BackColor = System.Drawing.SystemColors.MenuBar
+        Me.TopMostMenu.Checked = True
+        Me.TopMostMenu.CheckOnClick = True
+        Me.TopMostMenu.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.TopMostMenu.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.TopMostMenu.Name = "TopMostMenu"
+        Me.TopMostMenu.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.A), System.Windows.Forms.Keys)
+        Me.TopMostMenu.Size = New System.Drawing.Size(244, 26)
+        Me.TopMostMenu.Text = "总在最前"
+        '
+        'MenuStrip
+        '
+        Me.MenuStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolMenu, Me.帮助ToolStripMenuItem})
+        Me.MenuStrip.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip.Name = "MenuStrip"
+        Me.MenuStrip.Size = New System.Drawing.Size(393, 30)
+        Me.MenuStrip.TabIndex = 8
+        Me.MenuStrip.Text = "MenuStrip1"
+        '
+        '帮助ToolStripMenuItem
+        '
+        Me.帮助ToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutMenuItem})
+        Me.帮助ToolStripMenuItem.Font = New System.Drawing.Font("SimSun", 13.0!)
+        Me.帮助ToolStripMenuItem.Name = "帮助ToolStripMenuItem"
+        Me.帮助ToolStripMenuItem.Size = New System.Drawing.Size(66, 26)
+        Me.帮助ToolStripMenuItem.Text = "帮助"
+        '
+        'AboutMenuItem
+        '
+        Me.AboutMenuItem.Name = "AboutMenuItem"
+        Me.AboutMenuItem.Size = New System.Drawing.Size(152, 26)
+        Me.AboutMenuItem.Text = "关于"
+        '
+        'WTimeForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 17.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.ClientSize = New System.Drawing.Size(393, 203)
+        Me.Controls.Add(Me.MenuStrip)
+        Me.Controls.Add(Me.Panel1)
+        Me.Font = New System.Drawing.Font("Courier New", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MainMenuStrip = Me.MenuStrip
+        Me.MaximizeBox = False
+        Me.Name = "WTimeForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "计时器 Ver0.1"
+        Me.TopMost = True
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.MenuStrip.ResumeLayout(False)
+        Me.MenuStrip.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Public WithEvents wTimer As System.Windows.Forms.Timer
+    Friend WithEvents startButton As System.Windows.Forms.Button
+    Friend WithEvents periodText As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents progressBar As System.Windows.Forms.ProgressBar
+    Friend WithEvents timeView As System.Windows.Forms.TextBox
+    Friend WithEvents minuteView As System.Windows.Forms.TextBox
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents ToolMenu As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents TopMostMenu As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents MenuStrip As System.Windows.Forms.MenuStrip
+    Friend WithEvents 帮助ToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents AboutMenuItem As System.Windows.Forms.ToolStripMenuItem
+
+End Class
---
+++ WTime/Form1.vb
@@ -0,0 +1,148 @@
+﻿Public Class WTimeForm
+
+    Protected totalSecond As VariantType
+    Protected count As Integer
+    Protected timeString As String
+
+    Protected timeViewSetFlag As Integer
+
+    Protected thisText As String
+
+    Private Sub timer_begin()
+        If IsNumeric(periodText.Text) Then
+            totalSecond = periodText.Text
+            If totalSecond > 0 Then
+                count = -1
+
+                progressBar.Maximum = totalSecond
+                progressBar.Minimum = 0
+                progressBar.Value = 0
+
+                timeViewSetFlag = 1
+            End If
+        End If
+    End Sub
+
+    Private Sub display_title()
+        Dim ddd As Double
+        Dim eee As Double
+
+        Dim fff As String
+        Dim ggg As String
+
+        ddd = count / 60
+        eee = count Mod 60
+
+        ddd = Fix(ddd)
+        eee = Fix(eee)
+
+        fff = Convert.ToInt32(ddd)
+        If fff.Length < 2 Then
+            fff = "0" + fff
+        End If
+        ggg = Convert.ToInt32(eee)
+        If ggg.Length < 2 Then
+            ggg = "0" + ggg
+        End If
+
+        minuteView.Text = fff + ":" + ggg
+    End Sub
+
+    Private Sub timer_period()
+        count = count + 1
+
+        If (count > totalSecond) Then
+            count = 1
+            progressBar.Value = 0
+        End If
+
+        If count < 10 Then
+            timeString = "00" + count.ToString
+        ElseIf count < 100 Then
+            timeString = "0" + count.ToString
+        Else
+            timeString = count.ToString
+        End If
+
+        timeView.Text = timeString
+        If (count > 0) Then
+            progressBar.Value = count
+        End If
+
+        ' update GUI minute format
+        display_title()
+    End Sub
+
+    Private Sub timeViewChange()
+        If IsNumeric(timeView.Text) Then
+            If timeView.Text - 1 < totalSecond Then
+                count = timeView.Text - 1
+                timer_period()
+
+                periodText.Focus()
+                periodText.Select(0, periodText.TextLength)
+                timeViewSetFlag = 1
+            End If
+        End If
+    End Sub
+
+    Private Sub wTimer_Tick(ByVal sender As Object, ByVal e As System.EventArgs) Handles wTimer.Tick
+        If timeViewSetFlag = 1 Then
+            timer_period()
+        End If
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles startButton.Click
+        timer_begin()
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        timer_begin()
+        thisText = Text
+    End Sub
+
+    Private Sub periodText_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles periodText.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            timer_begin()
+        End If
+    End Sub
+
+    Private Sub timeView_GotFocus(ByVal sender As Object, ByVal e As System.EventArgs) Handles timeView.GotFocus
+        timeViewSetFlag = 0
+    End Sub
+
+    Private Sub timeView_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles timeView.KeyPress
+        If e.KeyChar < "0"c Or e.KeyChar > "9"c Then
+            e.KeyChar = ""
+        End If
+    End Sub
+
+    Private Sub timeView_Leave(ByVal sender As Object, ByVal e As System.EventArgs) Handles timeView.Leave
+        timeViewChange()
+    End Sub
+
+    Private Sub TopMostMenu_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TopMostMenu.Click
+        If Me.TopMost Then
+            Me.TopMost = False
+        Else
+            Me.TopMost = True
+        End If
+    End Sub
+
+    Private Sub periodText_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles periodText.KeyPress
+        If e.KeyChar < "0"c Or e.KeyChar > "9"c Then
+            e.KeyChar = ""
+        End If
+    End Sub
+
+    Private Sub AboutMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles AboutMenuItem.Click        
+        Dim msg As String
+        Dim title As String
+        Dim style As MsgBoxStyle
+        Dim response As MsgBoxResult
+        msg = "Version 0.1"
+        style = MsgBoxStyle.DefaultButton2 Or MsgBoxStyle.OkOnly
+        title = "关于"
+        response = MsgBox(msg, style, title)
+    End Sub
+End Class
